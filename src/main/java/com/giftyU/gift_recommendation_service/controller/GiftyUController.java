74e0569d563664babe7742669a9bb58fd2846b53
mahim.agg09@gmail.com <mahim.agg09@gmail.com>
2024-07-04 18:07:47 +0530

    Changing visibility

--- src/main/java/com/giftyU/gift_recommendation_service/controller/GiftyUController.java
+++ src/main/java/com/giftyU/gift_recommendation_service/controller/GiftyUController.java
@@ -1,5 +1,6 @@
 package com.giftyU.gift_recommendation_service.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,18 +14,18 @@ public class GiftyUController {
 	
 	
 	@PostMapping("/getReccomendation")
-	String newEmployee(@RequestBody RecommendationDetails recommendationDetails) {
+	ResponseEntity<String>  newEmployee(@RequestBody RecommendationDetails recommendationDetails) {
 		
 		if(StringUtils.isNotBlank(recommendationDetails.getOccasion())) {
 			if(recommendationDetails.getOccasion().equalsIgnoreCase("Anniversary")) {
-				return "Couples Watch";
+				return  ResponseEntity.ok("Couples Watch");
 			}else if(recommendationDetails.getOccasion().equalsIgnoreCase("Birthday")) {
-				return "Flowers with Cake";
+				return ResponseEntity.ok("Flowers with Cake");
 			}else if(recommendationDetails.getOccasion().equalsIgnoreCase("Farewell")) {
-				return "Amazon Gift Card";
+				return ResponseEntity.ok("Amazon Gift Card");
 			}
 		}
-		return "Sorry couldn't find the gift for this occasion :-(";
+		return ResponseEntity.ok("Sorry couldn't find the gift for this occasion :-(");
 	  }
 
 
