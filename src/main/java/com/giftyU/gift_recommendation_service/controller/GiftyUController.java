e3792dfe2964257dd91049b4abcfd154c3e31092
mahim.agg09@gmail.com <mahim.agg09@gmail.com>
2024-07-04 18:16:42 +0530

    Json Form

--- src/main/java/com/giftyU/gift_recommendation_service/controller/GiftyUController.java
+++ src/main/java/com/giftyU/gift_recommendation_service/controller/GiftyUController.java
@@ -1,10 +1,12 @@
 package com.giftyU.gift_recommendation_service.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.giftyU.gift_recommendation_service.model.RecommendationDetails;
 
 import io.micrometer.common.util.StringUtils;
@@ -12,20 +14,20 @@ import io.micrometer.common.util.StringUtils;
 @RestController
 public class GiftyUController {
 	
-	
 	@PostMapping("/getReccomendation")
 	ResponseEntity<String>  newEmployee(@RequestBody RecommendationDetails recommendationDetails) {
 		
 		if(StringUtils.isNotBlank(recommendationDetails.getOccasion())) {
 			if(recommendationDetails.getOccasion().equalsIgnoreCase("Anniversary")) {
-				return  ResponseEntity.ok("Couples Watch");
+				
+				return  ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("Couples Watch");
 			}else if(recommendationDetails.getOccasion().equalsIgnoreCase("Birthday")) {
-				return ResponseEntity.ok("Flowers with Cake");
+				return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("Flowers with Cake");
 			}else if(recommendationDetails.getOccasion().equalsIgnoreCase("Farewell")) {
-				return ResponseEntity.ok("Amazon Gift Card");
+				return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("Amazon Gift Card");
 			}
 		}
-		return ResponseEntity.ok("Sorry couldn't find the gift for this occasion :-(");
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("Sorry couldn't find the gift for this occasion :-(");
 	  }
 
 
